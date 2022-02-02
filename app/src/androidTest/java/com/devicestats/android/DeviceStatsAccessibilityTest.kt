769325ab32ef8321671151cae1aad319bce1a3bf
Scott Alexander-Bown <scottyab@users.noreply.github.com>
2022-02-02 16:44:19 +0000

    Added run with AndroidJUnit4

--- app/src/androidTest/java/com/devicestats/android/DeviceStatsAccessibilityTest.kt
+++ app/src/androidTest/java/com/devicestats/android/DeviceStatsAccessibilityTest.kt
@@ -3,10 +3,13 @@ package com.devicestats.android
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class DeviceStatsAccessibilityTest {
 
     @get:Rule
